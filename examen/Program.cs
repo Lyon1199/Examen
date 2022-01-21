39bdbb8475630b09ca39cacb6717851658a04146
Lyon1199 <lucasluis024@gmail.com>
2022-01-20 21:40:28 -0500

    se agrega el proyecto

---
+++ examen/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace examen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Singleton.Instance.mensaje);
+
+            Singleton.Instance.mensaje = "LISTA DE ESTUDIANTES DE CURSO";
+            Singleton.Instance.mensaje = "Estudiante 1: Juan";
+            Console.WriteLine(Singleton.Instance.mensaje);
+            // muestra el nombre del primer estudiante
+
+            Singleton.Instance.mensaje = "Estudiante 2: Liam";
+            Console.WriteLine(Singleton.Instance.mensaje);
+            // muestra el nombre del segundo estudiante
+
+            Singleton.Instance.mensaje = "Estudiante 3: Fernando";
+            Console.WriteLine(Singleton.Instance.mensaje);
+            // muestra el nombre del tercer estudiante
+
+            Singleton.Instance.mensaje = "Estudiante 4: Xavier";
+            Console.WriteLine(Singleton.Instance.mensaje);
+            // muestra el nombre del cuarto estudiante
+
+            Singleton.Instance.mensaje = "Estudiante 5: Edward";
+            Console.WriteLine(Singleton.Instance.mensaje);
+            //muestra el nombre del quinto estudiante
+
+            Singleton.Instance.mensaje = "Estudiante 6: Julio";
+            Console.WriteLine(Singleton.Instance.mensaje);
+            // muestra el nombre del sexto estudiante 
+
+            Singleton.Instance.mensaje = "Estudiante 7: Aida";
+            Console.WriteLine(Singleton.Instance.mensaje);
+            //muestra el nombre del septimo estudiante
+
+            Singleton.Instance.mensaje = "Estudiante 8: Carmen";
+            Console.WriteLine(Singleton.Instance.mensaje);
+            //muestra el nombre del octavo estudiante
+
+            Singleton.Instance.mensaje = "Estudiante 9: Sonia";
+            Console.WriteLine(Singleton.Instance.mensaje);
+            //muestra el nombre del noveno estudiante
+
+            Singleton.Instance.mensaje = "Estudiante 10: Liliana";
+            Console.WriteLine(Singleton.Instance.mensaje);
+            //muestra el nombre del decimo estudiante
+
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ examen/Singleton.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace examen
+{
+   
+        public class Singleton
+        {
+            private static Singleton instance = null;
+            public string mensaje = "";
+
+            protected Singleton()
+            {
+            mensaje = "LISTA DE ESTUDIANTE DE CURSO";
+
+            }
+
+            public static Singleton Instance
+            {
+                get
+                {
+                    if (instance == null)
+                        instance = new Singleton();
+
+                    return instance;
+                }
+            }
+
+        }
+}
+
